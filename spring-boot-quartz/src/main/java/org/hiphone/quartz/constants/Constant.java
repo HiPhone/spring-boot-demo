b7eef04d40568dc3f0bd99e538da182804d744b5
HiPhone <zhyzyhf@gmail.com>
2018-12-06 23:27:40 +0800

    添加quartz的demo

---
+++ spring-boot-quartz/src/main/java/org/hiphone/quartz/constants/Constant.java
@@ -0,0 +1,26 @@
+package org.hiphone.quartz.constants;
+
+/**
+ * @author HiPhone
+ */
+public class Constant {
+
+    public static final Integer TIMEOUT_VALUE = 5000;
+
+    public static final String SWAGGER_BASE_PACKAGE = "org.hiphone.quartz";
+
+    public static final String SWAGGER_TITLE = "Spring-Boot-Demo RESTful API 文档";
+
+    public static final String SWAGGER_DESCRIPTION = "Spring boot quartz";
+
+    public static final String SWAGGER_SERVICE_URL = "http://localhost:8080";
+
+    public static final String SWAGGER_CONTACT_NAME = "HiPhone";
+
+    public static final String SWAGGER_CONTACT_URL = "https://github.com/HiPhone";
+
+    public static final String SWAGGER_CONTACT_EMAIL = "zhyzyhf@gmail.com";
+
+    public static final String SWAGGER_VERSION = "1.0.0";
+
+}
---
+++ spring-boot-quartz/src/main/java/org/hiphone/quartz/controller/TestController.java
@@ -0,0 +1,36 @@
+package org.hiphone.quartz.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.hiphone.quartz.constants.ReturnCode;
+import org.hiphone.quartz.entitys.ResultMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HiPhone
+ */
+@Slf4j
+@RestController
+@Api(value = "TestController", description = "用于测试服务是否存活的Controller")
+public class TestController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @ResponseBody
+    @GetMapping("/echo-test")
+    @ApiOperation(value = "用于确认服务是否存活的接口", notes = "返回自身状态")
+    public ResultMessage test() {
+        log.info("Receive a request for testing " + applicationName);
+
+        return new ResultMessage(
+                ReturnCode.SUCCESS.getCode(),
+                ReturnCode.SUCCESS.getMessage(),
+                applicationName + " is alive");
+    }
+
+}
