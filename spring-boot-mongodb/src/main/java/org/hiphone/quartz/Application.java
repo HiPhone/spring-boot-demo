1cf58b2d5bf2d78ccd2b5f050df7d73038db96f7
HiPhone <zhyzyhf@gmail.com>
2018-12-07 23:12:16 +0800

    mongodb初始化

---
+++ spring-boot-mongodb/src/main/java/org/hiphone/quartz/Application.java
@@ -0,0 +1,15 @@
+package org.hiphone.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HiPhone
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
